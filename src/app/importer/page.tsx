4b6b2b60d0b35ac705c02e97f4f62fe31c33de7b
shivam <shivam@southpawstudio.co>
2024-03-12 15:00:59 +0530

    import csv

--- src/app/importer/page.tsx
+++ src/app/importer/page.tsx
@@ -1,13 +1,39 @@
-import Image from "next/image";
-
+'use client';
+import { CSVBoxButton } from '@csvbox/react'
 export default function Importer() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      {/* <h1>CSV Importer</h1> */}
       <div className="mt-6 flex max-w-md gap-x-4">
-       <h1>CSV Importer</h1>
+        <div className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+
+          <CSVBoxButton
+
+            licenseKey="10fnhBr1EiUkX7bAJBYq54fbLA04Xo"
+            user={{
+              user_id: "default123"
+            }}
+
+            onImport={(result: boolean, data: any) => {
+              if (result) {
+                console.log("success");
+                console.log(data.row_success + " rows uploaded");
+                //custom code
+              } else {
+                console.log("fail");
+                //custom code
+              }
+            }}
+            render={(launch: any, isLoading: boolean) => {
+              return <button disabled={isLoading} onClick={launch}>Upload file</button>;
+            }}
+          >
+            Import
+          </CSVBoxButton>
+
+        </div>
       </div>
-     
+
 
     </main>
   );
